763ae478007b2cf82ab168a5c126e36bb6df01bb
grey4uk <grey4uk14@gmail.com>
2020-05-15 18:06:39 +0300

    Updates

---
+++ precache-manifest.c816d6e2578c9e54ef8db2a52750c242.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bedac806a3a3036e9306cafb92ca448",
+    "url": "/labirynth_game/index.html"
+  },
+  {
+    "revision": "b77328aa525d293ec2eb",
+    "url": "/labirynth_game/static/css/main.14f96897.chunk.css"
+  },
+  {
+    "revision": "e0cc86924a8ebed7d122",
+    "url": "/labirynth_game/static/js/2.8f33aeef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/labirynth_game/static/js/2.8f33aeef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b77328aa525d293ec2eb",
+    "url": "/labirynth_game/static/js/main.672d5d50.chunk.js"
+  },
+  {
+    "revision": "2e4b493996d2557c7bd0",
+    "url": "/labirynth_game/static/js/runtime-main.c129481e.js"
+  }
+]);
